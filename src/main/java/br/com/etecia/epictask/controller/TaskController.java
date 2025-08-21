8fd897e4ae03718ca56ed3b4e0d428067993d46b
xyztavo <gustavobreasy@gmail.com>
2025-08-20 22:08:40 -0300

    feat: add attribute

--- src/main/java/br/com/etecia/epictask/controller/TaskController.java
+++ src/main/java/br/com/etecia/epictask/controller/TaskController.java
@@ -3,6 +3,7 @@ package br.com.etecia.epictask.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,14 +14,14 @@ import br.com.etecia.epictask.model.Task;
 public class TaskController {
 
     @GetMapping
-    public String listTasks() {
+    public String listTasks(Model model) {
         var tasks = List.of(
-            
-        new Task(1L, "Criar banco de dados", "Criar banco de dados mysql",50, 0);
-        new Task(2L, "Prototipo", "Montar o figma",40, 0);
-        new Task(3L, "Deploy", "Colocar em producao",100, 0);
-        
+        new Task(1L, "Criar banco de dados", "Criar banco de dados mysql",50, 0),
+        new Task(2L, "Prototipo", "Montar o figma",40, 0),
+        new Task(3L, "Deploy", "Colocar em producao",100, 0)
         );
+
+        model.addAttribute("tasks", tasks);
 
         return "tasks";
     }
